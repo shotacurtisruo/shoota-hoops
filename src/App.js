55cdf1b56d3f5d48783f3bd1bee317d22f23e373
Shota Ruo <scruo@ucdavis.edu>
2024-07-17 20:40:28 -0700

    shoota-hoops bento grid and hover effect on grids

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='col-left'>
+        <div className='row-row1'></div>
+        <div className='row-row2'></div>
+        <div className='row-row3'></div>
+      </div>
+      <div className='col-right'></div>
     </div>
   );
 }
