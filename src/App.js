1686c86c2d51909a0e83507101a6ac95b2fb94cf
Shota Ruo <scruo@ucdavis.edu>
2024-07-20 17:40:07 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="container">
       <div className='col-left'>
-        <div className='row-row1'></div>
-        <div className='row-row2'></div>
-        <div className='row-row3'></div>
+        <div className='row-row1'>
+          <h1 id='header'>Welcome to Shoota-hoops</h1>
+
+          <p>Interested in free basketball lessons for your kids? <br></br> For (Ages 8+ - High School)
+            <br></br>
+            Want to improve your skills?
+            <br></br>
+            Your at the right place!
+            
+            
+            
+          </p>
+          <p id='important'>Please read through carefully!</p>
+        </div>
+        <div className='row-row2'>
+          <p>
+            If interested please shoot me a text saying that you are interested and  please go over this form attached below:<br></br>
+            If you want individual training, please shoot me a text or call me. Prices will vary.
+          </p>
+
+        </div>
+
+
+
+
+        <div className='row-row3'>
+          <h1>Contact info:</h1>
+          <p>Please feel free to reach out via text, phone call or email!
+            <br></br>
+            Email: <a href="scruo@ucdavis.edu">scruo@ucdavis.edu</a>
+            <br></br>
+            Phone: 1-707-721-9987
+            <br></br>
+            Linkedln: <a href="https://www.linkedin.com/in/shota-ruo-1869b7244/">More About Me!</a>
+          </p>
+          
+
+          
+        </div>
       </div>
-      <div className='col-right'></div>
+      <div className='col-right'>
+        <h1>Hi this is Shota.</h1>
+        <p>I am currently a student at UC Davis studying computer science. This might make you wonder - well then why am I offering free basketball lessons? As a student, I am often spending my time coding or studying. However my love for basketball still lingers.  My passion for basketball started during middle school, which then eventually led to me playing during high school. Now in college, basketball no longer could be a priority due to academics. 
+          So I thought to myself: how can I do both? This is where I came up
+          with the idea of providing free lessons for the youth and community while using my web developing skills to create this page. 
+          I truly believe that basketball is more than just a game; it's a powerful medium that forges lifelong friendships, teaching teamwork, trust, and camaraderie both on and off the court.
+        </p>
+       
+
+
+      </div>
     </div>
   );
 }
