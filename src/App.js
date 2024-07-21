3ee6a412f233e11a5e9251f8ae9f4faf14ead083
Shota Ruo <scruo@ucdavis.edu>
2024-07-20 17:40:19 -0700

    no change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import './glow.css';
 
 
 function App() {
+  useEffect(() => {
+    const glowContainer = document.querySelector('.glow-container');
+
+    function getRandomShape() {
+      const shapes = ['50%', '0%', '25%'];
+      return shapes[Math.floor(Math.random() * shapes.length)];
+    }
+
+    function createGlow() {
+      const glow = document.createElement('div');
+      glow.classList.add('glow');
+      glow.style.top = `${Math.random() * 100}%`;
+      glow.style.left = `${Math.random() * 100}%`;
+      glow.style.width = `${Math.random() * 100 + 50}px`;
+      glow.style.height = glow.style.width;
+      glow.style.borderRadius = getRandomShape();
+      glowContainer.appendChild(glow);
+
+      setTimeout(() => {
+        glow.remove();
+      }, 4000); // Duration of the animation + buffer
+
+      setTimeout(createGlow, Math.random() * 2000 + 1000); // Create a new glow at random intervals
+    }
+
+    createGlow();
+  }, []);
   return (
     <div className="container">
+      <div className="glow-container"></div>
       <div className='col-left'>
         <div className='row-row1'>
           <h1 id='header'>Welcome to Shoota-hoops</h1>
 
-          <p>Interested in free basketball lessons for your kids? <br></br> For (Ages 8+ - High School)
+          <p>Interested in free basketball lessons for your kids? <br></br> For (Ages 10 and up to High School)
             <br></br>
             Want to improve your skills?
             <br></br>
@@ -22,17 +52,26 @@ function App() {
         </div>
         <div className='row-row2'>
           <p>
-            If interested please shoot me a text saying that you are interested and  please go over this form attached below:<br></br>
-            If you want individual training, please shoot me a text or call me. Prices will vary.
-          </p>
+            If interested please shoot me a text and  please go over this form attached below:<br></br>
+            If you are looking for individual training, please shoot me a text or call me. Prices will vary.
+            <br></br>
+            
+            <a href='https://docs.google.com/document/d/1FCVs77eJlRshRJgtyXuOPdq1NY-hFEDukl9rFMRgwXw/edit?usp=sharing'>Shoota-Hoops General Info</a>
+            <br></br>
+            <a href='https://docs.google.com/document/d/1bi4CP3ZyI6bIH4OHoFVz0rseN2N-I5S-gFTX9PZb1hA/edit?usp=sharing'>Waiver Form</a>
 
+           
+            
+
+          </p>
+          
         </div>
 
 
 
 
         <div className='row-row3'>
-          <h1>Contact info:</h1>
+          <h2>Contact info:</h2>
           <p>Please feel free to reach out via text, phone call or email!
             <br></br>
             Email: <a href="scruo@ucdavis.edu">scruo@ucdavis.edu</a>
@@ -53,8 +92,7 @@ function App() {
           with the idea of providing free lessons for the youth and community while using my web developing skills to create this page. 
           I truly believe that basketball is more than just a game; it's a powerful medium that forges lifelong friendships, teaching teamwork, trust, and camaraderie both on and off the court.
         </p>
-       
-
+        
 
       </div>
     </div>
